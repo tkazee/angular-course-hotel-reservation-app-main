ae0d1182b08b4360427309e884eb27c542577938
Azeem <Azeemtk9@gmail.com>
2024-12-31 14:31:44 +0530

    remove all local store calss from reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -9,8 +9,7 @@ export class ReservationService {
   private reservations: Reservation[] = [];
 
   constructor(){
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+    
   }
 
   // CRUD
@@ -28,19 +27,19 @@ export class ReservationService {
     reservation.id = Date.now().toString();
 
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+    //localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index,1)
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+    //localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+    //localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
   
 }
